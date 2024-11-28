33044686e307b38129fd9973091b5a49c2dc0c21
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-11-28 09:14:57 +0530

    db connection established

--- home.py
+++ home.py
@@ -1,5 +1,10 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+import sqlite3
+con=sqlite3.connect("/home/novavi/Documents/roshan/flask/dbs.db")
+try:
+    con.execute("create table std(name text,age int,email email)")
+except:
+    pass
 app=Flask(__name__)
 
 @app.route('/')
@@ -10,12 +15,22 @@ def Home():
 def Home1():
     return "Rise up"
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def Index():
+    if request.method=='POST':
+        name=request.form.get('name')
+        age=request.form.get('age')
+        email=request.form.get('email')
+        print(name,age,email)
+        con=sqlite3.connect("/home/novavi/Documents/roshan/flask/dbs.db")
+        con.execute("insert into std(name,age,email)values(?,?,?)",(name,age,email))
+        con.commit()
     return render_template('index.html')
 
 @app.route('/sec')
 def sec():
     return render_template('sec.html')
+
+
 
 app.run()
